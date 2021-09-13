a973a28365d4b9df745e703f306c3e0fef180beb
RickyMcK <84729289+RickyMcK@users.noreply.github.com>
2021-09-13 12:54:44 -0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  credentials = "${file("service-account")}"
+  project = "just-amp-325919"
+  region  = "us-central1"
+}
+
+resource "google_storage_instance" "vm_instance" {
+  name     = "demo4"
+  machine  = "f1-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+  }
+}
